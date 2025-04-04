41dc25388f986bce342fb0c61fb0733f66379d93
Abdoullahi Isse <119604973+Abdoullahi-dev@users.noreply.github.com>
2025-04-04 13:01:36 +0300

    update

--- Lab4StringExtensions/Class1.cs
+++ Lab4StringExtensions/Class1.cs
@@ -2,12 +2,13 @@
 {
     public static class StringExtensions
     {
-        public static bool StartsWithUpperCase(this string str)
+        public static bool StartsWithUpper(this string str)
         {
-            if (string.IsNullOrEmpty(str))
+            if (string.IsNullOrWhiteSpace(str))
                 return false;
 
-            return char.IsUpper(str[0]);
+            char ch = str[0];
+            return char.IsUpper(ch);
         }
     }
 }
